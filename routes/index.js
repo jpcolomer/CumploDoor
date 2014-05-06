7b442e701883aae77165a0f8585add97296093ca
jpcolomer <jpcolomer@gmail.com>
2014-05-06 13:30:59 -0400

    Include gpio on production only

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var gpio = require('pi-gpio');
+if (process.env.NODE_ENV === 'production'){
+  var gpio = require('pi-gpio');
+}
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -8,17 +10,19 @@ router.get('/', function(req, res) {
 });
 
 router.post('/abrir', function(req, res){
-  gpio.open(11, "output", function(err) {
-    return gpio.write(11, 1, function() {
-      return setTimeout(closeOutput, 500);
+  if (process.env.NODE_ENV === 'production') {
+    gpio.open(11, "output", function(err) {
+      return gpio.write(11, 1, function() {
+        return setTimeout(closeOutput, 500);
+      });
     });
-  });
 
-  closeOutput = function() {
-    return gpio.write(11, 0, function() {
-      return gpio.close(11);
-    });
-  };
+    closeOutput = function() {
+      return gpio.write(11, 0, function() {
+        return gpio.close(11);
+      });
+    };
+  }
   res.redirect('/');
 });
 module.exports = router;
